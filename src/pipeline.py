6fc0ee25ffea5b4e08e35822fe0bd6bc6ea6bb96
Dstra <64135655+DWJStraat@users.noreply.github.com>
2023-09-25 13:52:34 +0200

    Added docstrings

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,18 +1,55 @@
+"""
+A pipeline for building and searching HMMs.
+Written by: David Straat
+Date: 25-sept-2023
+"""
 import os
 
+
 class Pipeline:
-    def __init__(self, file,database):
+    """
+    This class is a pipeline for building and searching HMMs.
+    """
+
+    def __init__(self, file, database, iterations=1):
         self.blast = None
         self.file = file
         self.database = database
+        self.iter = iterations
         self.msa_file = f'{self.file}-msa.fna'
 
     def align(self):
+        """
+        Aligns the sequences in the file using MAFFT.
+        """
         os.system(f'mafft {self.file} > {self.msa_file}')
 
     def hmmbuild(self):
+        """
+        Builds a HMM from the aligned sequences.
+        """
         os.system(f'hmmbuild {self.file}.hmm {self.msa_file}')
 
     def hmmsearch(self):
+        """
+        Searches the database for sequences that match the HMM.
+        """
         os.system(f'hmmsearch --tblout {self.file}-output.txt '
                   f'{self.file}.hmm {self.database}')
+
+    def run(self, iterations=None):
+        """
+        Runs the pipeline.
+        """
+        if iterations is not None:
+            self.iter = iterations
+        for _ in range(self.iter):
+            self.loop()
+
+    def loop(self):
+        """
+        Runs the pipeline once.
+        """
+        self.align()
+        self.hmmbuild()
+        self.hmmsearch()
